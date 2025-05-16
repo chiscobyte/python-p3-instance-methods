94346556d090b883a41c0b18296afdb1cd31a669
chiscobyte <chiscoenock@gmail.com>
2025-05-16 18:14:39 +0300

    initiate

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python3
+from lib.person import Person
 
 class Dog:
     # Class body goes here
+    def __init__(self):
+        pass
 
-    #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.") 
+    def walk(self):
+        print("The person is walking.")  
+    def talk(self):
+        print("Hello World!") 
+
+
+
+fido = Dog()
+fido.bark()
+fido.sit()
+
+snoopy = Dog()
+snoopy.bark()
+
+person = Person()
+person.walk()
+person.talk()
+
